f5b08a263ccd164cd1bc572180e1ed6e228fe746
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-02 17:46:17 +0300

    ✨Added AllTripsMap component and update routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import TripDetailPage from './pages/TripDetailPage';
 import AuthPage from './pages/AuthPage';
 import DashboardPage from './pages/DashboardPage';
 import CreateTripPage from './pages/CreateTripPage';
-
+import AllTripsMap from './pages/AllTripsMap';
 
 function ProtectedRoute() {
     const { currentUser } = useAuth();
@@ -26,12 +26,13 @@ export default function App() {
   return (
     <AuthProvider>
       <div className="bg-gray-100 dark:bg-[#121212] min-h-screen transition-colors duration-500">
-        <Header theme={theme} onThemeToggle={handleThemeToggle} />
+        <Header />
         <AnimatePresence mode='wait'>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/trip/:tripId" element={<TripDetailPage />} />
             <Route path="/auth" element={<AuthPage />} />
+            <Route path="/explore-map" element={<AllTripsMap />} />
             
             <Route element={<ProtectedRoute />}>
               <Route path="/dashboard" element={<DashboardPage />} />
